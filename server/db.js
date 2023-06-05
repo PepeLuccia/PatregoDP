34dd8c90a7799d8f90b906b140c41f17bc4dd0a8
PepeLuccia <pashkadoo@gmail.com>
2023-06-05 08:05:13 +0300

    modified:   server/db.js 	modified:   src/views/add.ejs 	modified:   src/views/product.ejs

--- server/db.js
+++ server/db.js
@@ -10,35 +10,3 @@ const connection = mysql.createConnection({
 });
 
 module.exports = connection;
-
-//R.setup('mysql:host=localhost;dbname=patrego', 'root', '');
-
-/*const session = require('express-session');
-const express = require('express');
-const app = express();
-
-app.use(session({
-    secret: 'your-secret-key',
-    cookie: {
-        maxAge: 100 * 24 * 60 * 60 * 1000 // 100 days
-    }
-}));
-
-app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    next();
-});
-
-// Check if the user is not banned
-if (req.session.logged_user) {
-    // Check if the user is not banned
-    const userCount = R.count('users', 'id = ? and status = 0', [req.session.logged_user.id]);
-    if (userCount) {
-        // Destroy session
-        req.session.destroy();
-        // Redirect to the main page
-        res.redirect('/patregodp/?banned');
-    }
-} else if (!req.LOGIN) {
-    res.redirect('/patregodp/');
-} */
